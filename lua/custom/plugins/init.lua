861cb357e67e4066c3c5e2c642ac15dad7a6aa79
Michael Carver <4365312+shortcarver@users.noreply.github.com>
2025-03-10 20:58:43 -0500

    trying out some themes

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -51,15 +51,35 @@ return {
   --     vim.cmd 'colorscheme rose-pine'
   --   end,
   -- },
-  {
-    'nvim-tree/nvim-web-devicons',
-  },
+  -- {
+  --   'folke/tokyonight.nvim',
+  --   priority = 1000, -- Make sure to load this before all the other start plugins.
+  --   init = function()
+  --     -- Load the colorscheme here.
+  --     -- Like many other themes, this one has different styles, and you could load
+  --     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+  --     vim.cmd.colorscheme 'tokyonight-storm'
+  --
+  --     -- You can configure highlights by doing something like:
+  --     vim.cmd.hi 'Comment gui=none'
+  --   end,
+  -- },
+  -- {
+  --   'catppuccin/nvim',
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd 'colorscheme catppuccin'
+  --   end,
+  -- },
   {
     'Mofiqul/vscode.nvim',
     name = 'vscode',
     config = function()
       vim.cmd 'colorscheme vscode'
     end,
+  },
+  {
+    'nvim-tree/nvim-web-devicons',
   },
   {
     'christoomey/vim-tmux-navigator',
