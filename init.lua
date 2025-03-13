6310da25e7137e2d90112fda14362115b769b000
Michael Carver <michael.carver@tpac.com>
2025-03-13 15:40:20 -0500

    added ctrl k for help

--- init.lua
+++ init.lua
@@ -459,6 +459,8 @@ require('lazy').setup({
           -- or a suggestion from your LSP for this to activate.
           map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
 
+          map('<c-k>', vim.lsp.buf.signature_help, 'show suggestions', 'i')
+
           -- WARN: This is not Goto Definition, this is Goto Declaration.
           --  For example, in C this would take you to the header.
           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
