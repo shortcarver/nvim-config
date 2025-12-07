8db84eb73fd6191a1d220487e1c4df7735e01a01
Michael Carver <4365312+shortcarver@users.noreply.github.com>
2025-12-06 23:15:51 -0600

    making the background transparent

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -95,6 +95,9 @@ return {
     'Mofiqul/vscode.nvim',
     name = 'vscode',
     config = function()
+      require('vscode').setup {
+        transparent = true,
+      }
       vim.cmd 'colorscheme vscode'
     end,
   },
